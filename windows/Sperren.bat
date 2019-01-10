14994911ceea8e11d5e20a46ec025adc82b2ed6b
Fabian Lemke <info@kikkirej.net>
2019-01-10 12:41:20 +0100

    Add files via upload

---
+++ windows/Sperren.bat
@@ -0,0 +1,23 @@
+@echo off
+echo 10
+timeout /T 1  > nul
+echo 9
+timeout /T 1  > nul
+echo 8
+timeout /T 1  > nul
+echo 7
+timeout /T 1  > nul
+echo 6
+timeout /T 1  > nul
+echo 5
+timeout /T 1  > nul
+echo 4
+timeout /T 1  > nul
+echo 3
+timeout /T 1  > nul
+echo 2
+timeout /T 1  > nul
+echo 1
+timeout /T 1  > nul
+
+rundll32.exe user32.dll, LockWorkStation
